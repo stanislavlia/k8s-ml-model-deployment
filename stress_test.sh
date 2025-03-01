48f99dc9672d33a8bd513b22daf52a32c8d8b4a0
Stanislav Liashkov <stanislavliashkov@gmail.com>
2025-03-01 17:22:45 +0700

    setup grafana

--- stress_test.sh
+++ stress_test.sh
@@ -1,4 +1,4 @@
 echo "============================================"
 echo "Stress tests using batchmode; size=256"
-ab -n 100 -c 1 -p input_examples/batch_256.json -T application/json -rk http://localhost:8011/prediction   
+ab -n 1000 -c 15 -p input_examples/batch_256.json -T application/json -rk http://localhost:8011/prediction   
 echo "DONE"
